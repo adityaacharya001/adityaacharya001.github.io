16c7e790c8c0e3b911300a6f874b05e081b1c0ae
adityaacharya001 <30295883+adityaacharya001@users.noreply.github.com>
2017-10-04 14:46:56 +0530

    done

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@
 
     //typed js
     $(".typed").typed({
-        strings: ["My Name is Aditya Acharya", "I'm a Web Designer", "Love Simplicity"],
+        strings: ["My Name is Aditya Acharya", "I'm a Web Designer"],
         typeSpeed: 100,
         backDelay: 900,
         // loop
